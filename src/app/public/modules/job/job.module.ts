14b561578322c9719c02e0a2ba16afdc4a5fab7f
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-09-11 20:47:26 -0400

    init uio card jobs component

--- src/app/public/modules/job/job.module.ts
+++ src/app/public/modules/job/job.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { JobsViewComponent } from './views/jobs-view/jobs-view.component';
 import {JobRoutesModule} from './routes/job-routes.module';
+import { JobCardComponent } from './components/job-card/job-card.component';
 
 @NgModule({
   declarations: [
-    JobsViewComponent
+    JobsViewComponent,
+    JobCardComponent
   ],
   imports: [
     CommonModule,
